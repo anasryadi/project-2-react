ce947073c3239d842fca4535495f0f66b0a2bc4f
Anas Ryadi <anasryadi@Anass-MBP.socal.rr.com>
2022-05-18 14:41:47 -0700

    changing the useEffect from app.js to NewsPage.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home";
 import NewsPage from "./Components/NewsPage"
 import { Link, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [news, setNews] = useState(null);
-
-  const apiKey = "ea42bb03df5c4e5f88941b56087889c2";
-  useEffect(() => {
-    fetch(
-      `https://newsapi.org/v2/everything?q=Apple&from=2022-05-13&apiKey=${apiKey}`
-    )
-      .then(response => response.json())
-      .then(data => setNews(data.articles))
-      // .then((data) => console.log(data))
-      .catch((error) => console.log("error"));
-  }, []);
-  console.log(news)
 
   return (
     <div className="App">
@@ -32,10 +17,47 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/news" element={<NewsPage news={news} />} />
+        <Route path="/news" element={<NewsPage />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+
+// function App() {
+//   const [news, setNews] = useState(null);
+
+//   const apiKey = "ea42bb03df5c4e5f88941b56087889c2";
+//   useEffect(() => {
+//     fetch(
+//       `https://newsapi.org/v2/everything?q=Apple&from=2022-05-13&apiKey=${apiKey}`
+//     )
+//       .then(response => response.json())
+//       .then(data => setNews(data.articles))
+//       // .then((data) => console.log(data))
+//       .catch((error) => console.log("error"));
+//   }, []);
+//   console.log(news)
+
+//   return (
+//     <div className="App">
+//       <nav>
+//         <Link to="/">
+//           <h1>home</h1>
+//         </Link>
+//         <Link to="/news">
+//           <h1>news</h1>
+//         </Link>
+//       </nav>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/news" element={<NewsPage news={news} />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/Components/NewsPage.js
+++ src/Components/NewsPage.js
@@ -1,21 +1,53 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-function NewsPage(props) {
-  let newsMap;
-  if (props.news !== null) {
-    newsMap = props.news.map((newsItem) => {
-      return <h1>{newsItem.title}</h1>;
+function NewsPage() {
+  const [news, setNews] = useState(null);
+
+  const apiKey = "ea42bb03df5c4e5f88941b56087889c2";
+
+  useEffect(() => {
+    fetch(
+      `https://newsapi.org/v2/everything?q=Apple&from=2022-05-13&apiKey=${apiKey}`
+    )
+      .then(response => response.json())
+      .then(data => setNews(data.articles))
+      .catch((error) => console.log("error"));
+  }, []);
+
+  const newsMap = news.map((newsItem) => {
+      return <li>{newsItem.title}</li>;
     });
-  }
-
-  console.log(props.news);
 
   return (
     <div>
       <h1>News</h1>
-      {newsMap}
+      <ul>{newsMap}</ul>
     </div>
   );
 }
 
 export default NewsPage;
+
+
+// function NewsPage(props) {
+
+//   let newsMap;
+//   if (props.news !== null) {
+//     newsMap = props.news.map((newsItem) => {
+//       // return <h1>{newsItem.title}</h1>;
+//       return <a href={newsItem.description}>{newsItem.title}</a>;
+//     });
+//   }
+
+//   console.log(props.news);
+
+//   return (
+//     <div>
+//       <h1>News</h1>
+//       {newsMap}
+//     </div>
+//   );
+// }
+
+// export default NewsPage;
