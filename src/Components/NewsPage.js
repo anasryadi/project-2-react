8989987060ccd64a8f39d66477e6b22ba2c67c39
Anas Ryadi <anasryadi@Anass-MBP.socal.rr.com>
2022-05-18 15:21:08 -0700

    added description and images to the news

--- src/Components/NewsPage.js
+++ src/Components/NewsPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function NewsPage() {
-  const [news, setNews] = useState(null);
+  const [news, setNews] = useState([]);
 
   const apiKey = "ea42bb03df5c4e5f88941b56087889c2";
 
@@ -10,14 +10,21 @@ function NewsPage() {
     fetch(
       `https://newsapi.org/v2/everything?q=Apple&from=2022-05-13&apiKey=${apiKey}`
     )
-      .then(response => response.json())
-      .then(data => setNews(data.articles))
+      .then((response) => response.json())
+      .then((data) => setNews(data.articles))
       .catch((error) => console.log("error"));
   }, []);
 
   const newsMap = news.map((newsItem) => {
-      return <li>{newsItem.title}</li>;
-    });
+    return (
+      <li>
+        {newsItem.title}
+        <p>{newsItem.description}</p>
+        <img src={newsItem.urlToImage} alt="News IMG"  width="900" height="600"></img>
+
+      </li>
+    );
+  });
 
   return (
     <div>
@@ -28,7 +35,6 @@ function NewsPage() {
 }
 
 export default NewsPage;
-
 
 // function NewsPage(props) {
 
